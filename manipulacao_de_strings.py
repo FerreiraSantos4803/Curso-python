c72754629ea88167371db267e1819fe716b31266
matheus <backupmatheus4803@gmail.com>
2024-03-12 21:27:14 -0300

    adicionei starswith

--- manipulacao_de_strings.py
+++ manipulacao_de_strings.py
@@ -1,8 +1,12 @@
-## exercitando os estudos do dia -> manipulação de strings
+# exercitando os estudos do dia -> manipulação de strings
 
 nome = "eu preciso de um trabalho na área"
 
-print(nome.upper()) #todos maiúsculas
-print(nome.title()) #só a primeira inicial
-print(nome[::-1]) #noem de trás para frente
-print(nome.center(14,"#"))
+print(nome.upper())  # todos maiúsculas
+print(nome.title())  # só a primeira inicial
+print(nome[::-1])  # noem de trás para frente
+print(nome.center(14, "#"))
+if nome.startswith("u"):
+    print(f'O nome começa com a letra {nome[0]}')
+else:
+    print(f'O nome começa com a letra {nome[0]}')
